ff2d44ff37a93a3e0ad1812db5cb247bb3f49cb6
root <andreascorrano@hotmail.it>
2025-01-07 16:32:08 +0000

    mini aggiunte salvataggio

--- planning/src/move.cpp
+++ planning/src/move.cpp
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/*
 #include <math.h>
 
 #include <memory>
@@ -199,7 +199,7 @@ int main(int argc, char ** argv)
 
 
 
-/*
+*/
 
 #include <memory>
 #include <algorithm>
@@ -257,6 +257,6 @@ int main(int argc, char ** argv)
 }
 
 
-*/
+
 
 
